51a84efdd46c14a83c4729de469151bda5b05e69
luanbrito <luan@viitra.com>
2020-09-30 17:19:27 -0300

    init node project and setup express server

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import http from 'http'
+
+const app = express()
+const server = http.createServer(app)
+
+app.get('/', function(req, res) {
+    res.send('hello world');
+});
+
+server.listen(3000, () => {
+    console.log("Server listening on port 3000.")
+})
