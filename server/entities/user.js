933a13c3c0722565453aee3a8ddf4b319574735a
luanbrito <lab7@cin.ufpe.br>
2020-10-07 19:41:39 -0300

    refactor: entities factory group functions

--- server/entities/user.js
+++ server/entities/user.js
@@ -1,30 +1,28 @@
 import User from '../schemas/user.js'
 
-export function getUserFactory() {
-    return async function getUser(githubId) {
-        let foundUser = undefined
-        await User.findOne({ '_id': githubId }, function (err, user) {
-            if (err) {
-                console.log(err)
-                return null
-                // return err
-            }
-            foundUser = user
-        })
-        return foundUser
-    }
-}
-
-export function createUserFactory() {
-    return function createUser({githubId, login}) {
-        const newUser = new User({
-            _id: githubId,
-            login: login
-        });
-        newUser.save(function (err) {
-            if (err) return null
+export default function UserFactory() {
+    return {
+        'getUser': async (githubId) => {
+            let foundUser = undefined
+            await User.findOne({ '_id': githubId }, function (err, user) {
+                if (err) {
+                    console.log(err)
+                    return null
+                }
+                foundUser = user
+            })
+            return foundUser
+        },
+        'createUser': async ({githubId, login}) => {
+            const newUser = new User({
+                _id: githubId,
+                login: login
+            });
+            newUser.save(function (err) {
+                if (err) return null
+                return newUser
+            })
             return newUser
-        })
-        return newUser
+        }
     }
 }
--- server/schemas/user.js
+++ server/schemas/user.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose'
-// const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     login: {
